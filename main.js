a2ccc5e5b77f4c25b3bba306187dc1df70af8fd0
yatoogamii <bracaval.elias.pro@gmail.com>
2019-04-24 23:26:33 +0200

    add lodash and capitalize function

--- main.js
+++ main.js
@@ -14,11 +14,15 @@ new Vue ({
     valueId: 1
   },
   methods: {
+    capitalize: function(value) {
+      return _.capitalize(value);
+    },
     addValueInArr: function() {
       if (this.value) {
+        
         this.arr.push({
           id: this.valueId++,
-          text: this.value
+          text: this.capitalize(this.value)
         });
         this.value = '';
       }
